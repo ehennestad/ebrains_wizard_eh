9817dbfc4fb61edb2ff24c9087ff60fa53b70bbc
Eivind Hennestad <eivinhen@1x-193-157-194-208.uio.no>
2022-11-03 13:17:53 +0100

    Updated contributors page based on suggestions from Ulrike and Ingrid

--- src/helpers/ErrorTransformer.js
+++ src/helpers/ErrorTransformer.js
@@ -2,6 +2,9 @@ export const transformErrors = errors => {
     return errors.map(error => {
         if (error.name === "required") {
             error.message = "Please fill out this field."
+        } else if (error.property === ".contributors" && error.name === "minItems") {
+            error.message = "Please add at least one contributor."
+
         }
         return error;
     });
