a3ba8201e7153e3fd158c84c2258ad7444c1a324
Eivind Hennestad <eivinhen@1x-193-157-194-208.uio.no>
2022-12-09 14:02:43 +0100

    Update ErrorTransformer.js
    Update error messages for the last page

--- src/helpers/ErrorTransformer.js
+++ src/helpers/ErrorTransformer.js
@@ -1,6 +1,24 @@
 export const transformErrors = errors => {
     return errors.map(error => {
-        if (error.name === "required") {
+
+        // Replace message if no contributors are added
+        if (error.property === ".contributors" && error.name === "minItems") {
+            error.message = "Please add at least one contributor."
+
+        // Replace message if no excel file with subjects is uploaded
+        } else if (error.property === ".subjectExpMetadata.uploadedExcelFile" && error.name === "required") {
+            error.message = "Please fill out and upload the SubjectMetadata excel file. If you don't have subjects, select 'No' in the above field instead."
+        
+        // Replace message if user said there are subjects but didn't upload an excel file
+        } else if (error.property === ".subjectExpMetadata.subjectsExist" && error.name === "const") {
+            error.message = "If you have subjects, please upload a filled out excel file below. Otherwise, select 'No'"
+        
+        // Remove on-sensible error message
+        } else if (error.property === ".subjectExpMetadata" && error.name === "oneOf") {
+            error.message = ""
+        
+        // Replace generic message for required fields
+        } else if (error.name === "required") {
             error.message = "Please fill out this field."
         }
         return error;
