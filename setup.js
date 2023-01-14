5c6c0b3cac86e8f3db2c9cb816055c8144212c7f
ehennestad <ehennestad@gmail.com>
2023-01-14 01:29:56 +0100

    Update setup.js
    remove performance.now

--- setup.js
+++ setup.js
@@ -4,16 +4,18 @@ const {execSync} = require('child_process');
 async function setup() {
     
     console.log('Fetching controlled terms...')
-    var startTime = performance.now()
+    //var startTime = performance.now()
     let result = await fetchControlledTerms();
-    var endTime = performance.now()
-    console.log(`Fetching controlled terms took: ${endTime - startTime} milliseconds`)
+    //var endTime = performance.now()
+    //console.log(`Fetching controlled terms took: ${endTime - startTime} milliseconds`)
+    console.log('Done fetching controlled terms.')
     
     console.log('Creating build...')
-    var startTime = performance.now()
+    //var startTime = performance.now()
     execSync('npm run build') // Redo the build in order for the updated terms to be used by the frontend
-    var endTime = performance.now()
-    console.log(`Creating build took: ${endTime - startTime} milliseconds`)
+    //var endTime = performance.now()
+    //console.log(`Creating build took: ${endTime - startTime} milliseconds`)
+    console.log('Done creating build.')
 }
 
 setup();
