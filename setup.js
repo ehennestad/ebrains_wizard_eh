bc7ae07977e6eaddf07fee1f55c828ece9f05f97
ehennestad <ehennestad@gmail.com>
2023-01-14 01:44:08 +0100

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,5 +1,5 @@
 var fetchControlledTerms = require('./server/kg_util/fetchControlledTerms');
-const {execSync} = require('child_process');
+const {exec} = require('child_process');
 
 async function setup() {
     
@@ -12,7 +12,7 @@ async function setup() {
     
     console.log('Creating build...')
     //var startTime = performance.now()
-    execSync('npm run build') // Redo the build in order for the updated terms to be used by the frontend
+    exec('npm run build') // Redo the build in order for the updated terms to be used by the frontend
     //var endTime = performance.now()
     //console.log(`Creating build took: ${endTime - startTime} milliseconds`)
     console.log('Done creating build.')
