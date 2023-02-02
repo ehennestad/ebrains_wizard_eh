1fbf190a5261a0135032f7f8d4a43f81901c527e
ehennestad <ehennestad@gmail.com>
2023-02-02 19:11:33 +0100

    Update setup.js
    Ensure build stage happens after all controlled terms are fetched

--- setup.js
+++ setup.js
@@ -1,21 +1,32 @@
 var fetchControlledTerms = require('./server/kg-util/fetchControlledTerms');
 const {exec} = require('child_process');
 
-async function setup() {
-    
-    console.log('Fetching controlled terms...')
-    //var startTime = performance.now()
-    let result = await fetchControlledTerms();
-    //var endTime = performance.now()
-    //console.log(`Fetching controlled terms took: ${endTime - startTime} milliseconds`)
-    console.log('Done fetching controlled terms.')
-    
-    console.log('Creating build...')
-    //var startTime = performance.now()
-    exec('npm run build') // Redo the build in order for the updated terms to be used by the frontend
-    //var endTime = performance.now()
-    //console.log(`Creating build took: ${endTime - startTime} milliseconds`)
-    console.log('Done creating build.')
-}
-
 setup();
+
+async function setup() {
+    // Fetch controlled terms from KG
+    // This is done before the React App is built so that the controlled terms are available
+
+    console.log('Fetching controlled terms...')
+    var startTime = performance.now()
+
+    fetchControlledTerms()
+        .then( () => {
+            var endTime = performance.now()
+            console.log(`Fetched all controlled terms in: ${(endTime - startTime)/1000} seconds`)
+
+            // Redo the build in order for the updated terms to be used by the frontend
+            console.log('Creating a production build for the React App...')           
+            exec('npm run build', (err) => {
+                if (err) {
+                    console.error(`exec error: ${err}`);
+                } else {
+                    console.log(`Completed React App build in: ${(performance.now() - endTime)/1000} seconds`);
+                }
+                //console.log(`Completed React App build in: ${(performance.now() - endTime)/1000} seconds`);
+            }); 
+        })
+        .catch(err => {
+            console.log('Error fetching controlled terms: ' + err);
+        })
+}
