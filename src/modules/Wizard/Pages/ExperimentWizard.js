fa9ba7c66c397f24ca0c0da4cacc2bd94898393c
ehennestad <ehennestad@gmail.com>
2023-02-07 22:51:35 +0100

    Update ExperimentWizard.js
    Add confirmation modal on experiment page

--- src/modules/Wizard/Pages/ExperimentWizard.js
+++ src/modules/Wizard/Pages/ExperimentWizard.js
@@ -9,25 +9,34 @@ import {uiSchema} from '../../../helpers/uiSchemaProvider';
 import {transformErrors} from '../../../helpers/ErrorTransformer';
 
 import CascadeSelector from '../../../components/CascadeSelector';
+import SubmissionConfirmationModal from '../../../components/SubmissionConfirmationModal';
 
 
-const ExperimentWizard = React.memo(({ schema, formData,  onSubmit, onChange, onError, goBack, formRef, isValid }) => {
+const ExperimentWizard = ( { schema, formData,  onSubmit, onChange, onError, goBack, formRef, isValid, doShowModal, onSubmissionConfirmed, onSubmissionCanceled} ) => {
 
   const handleOnChange = ( {formData} ) => onChange(formData);
   const handleOnSubmit = ( {formData} ) => onSubmit(formData);
 
-  return (
-    <Form widgets={{richtext: RichTextWidget}} fields={CustomArrayField} schema={schema} uiSchema={uiSchema} formData={formData} transformErrors={transformErrors} showErrorList={false} omitExtraData={true} onSubmit={handleOnSubmit} onChange={handleOnChange} onError={onError} validator={validator} ref={formRef}>
-      <div className="footer">
-        <div className="col-xs-5 back-panel">
-          <button type="submit" className="btn btn-info btn-default" onClick={goBack}>Previous Page</button>
-        </div>
-        <div className="col-xs-5 col-xs-offset-2 submit-panel">
-          {isValid ? <button type="submit" className="btn btn-info btn-primary">Submit Metadata</button> : <button type="submit" className="btn btn-info btn-primary" title="Please make sure all pages are filled out" disabled>Submit Metadata </button>}
-        </div>
-      </div>
-    </Form>
-  );
-});
+  console.log('doShowModal', doShowModal)
 
-export default ExperimentWizard;
+  return (
+    <>
+      <Form widgets={{richtext: RichTextWidget}} fields={CustomArrayField} schema={schema} uiSchema={uiSchema} formData={formData} transformErrors={transformErrors} showErrorList={false} omitExtraData={true} onSubmit={handleOnSubmit} onChange={handleOnChange} onError={onError} validator={validator} ref={formRef}>
+        <div className="footer">
+          <div className="col-xs-5 back-panel">
+            <button type="submit" className="btn btn-info btn-default" onClick={goBack}>Previous Page</button>
+          </div>
+          <div className="col-xs-5 col-xs-offset-2 submit-panel">
+            {isValid ? <button type="submit" className="btn btn-info btn-primary">Submit Metadata</button> : <button type="submit" className="btn btn-info btn-primary" title="Please make sure all pages are filled out" disabled>Submit Metadata</button>}
+          </div>
+        </div>
+      </Form>
+      <SubmissionConfirmationModal doShowModal={doShowModal} onOk={onSubmissionConfirmed} onCancel={onSubmissionCanceled} />
+    </>
+
+  );
+};
+
+const ExperimentWizardMemoized = React.memo( ExperimentWizard )
+export default ExperimentWizardMemoized;
+ 
