650f873c1075d55b69c5db3f99e347fcec439273
ehennestad <ehennestad@gmail.com>
2023-03-15 12:11:21 +0100

    Allow gif upload and set max image file size to 5 MB

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -55,16 +55,17 @@ const ImageUpload = ({oldFileList, onImageUploadedFcn}) => {
   // };
 
   const beforeUpload = (file) => {
-    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
-    if (!isJpgOrPng) {
-      message.error('You can only upload JPG/PNG file!');
+    const isValidImageFormat = file.type === 'image/jpeg' || file.type === 'image/png'; // Todo: add gif
+    if (!isValidImageFormat) {
+      message.error('You can only upload a JPG/PNG/GIF file!');
     }
     
-    const isLt2M = file.size / 1024 / 1024 < 5;
-    if (!isLt2M) {
+    const isLt5M = file.size / 1024 / 1024 < 5;
+    if (!isLt5M) {
       message.error('Image must smaller than 5MB!');
     }
-    return isJpgOrPng && isLt2M;
+
+    return isValidImageFormat && isLt5M;
   };
 
   const uploadButton = (
@@ -83,7 +84,7 @@ const ImageUpload = ({oldFileList, onImageUploadedFcn}) => {
         <ImgCrop quality={1} grid>
           <Upload
             action='/api/upload/previewImage'
-            accept=".png,.jpg"
+            accept=".png,.jpg,.gif"
             listType="picture"
             fileList={fileList}
             beforeUpload={beforeUpload}
