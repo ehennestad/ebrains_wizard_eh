227b235d02e3a7ec453e90c7bd4003e3edaefeb3
ehennestad <ehennestad@gmail.com>
2023-07-25 22:16:43 +0200

    Temp: get token from env

--- server/kg-util/getRequestOptions.js
+++ server/kg-util/getRequestOptions.js
@@ -19,6 +19,10 @@ async function getRequestOptions() {
 
 async function getTokenFromServiceAccount() {
 
+    // Temporary, as service account token stopped working:
+    return process.env.EBRAINS_ACCESS_TOKEN
+
+
     let endpointURL = "https://iam.ebrains.eu/auth/realms/hbp/protocol/openid-connect/token";
     let secret = process.env.OIDC_CLIENT_SECRET;
 
